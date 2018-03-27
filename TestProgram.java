b4017307850c940fa843dc80d0ec4f3a4dadacd3
SumithaM <sumithaamoorthi@gmail.com>
2018-03-27 15:24:36 +0530

    Maven project

--- TestProgram.java
+++ TestProgram.java
@@ -3,7 +3,7 @@ package com.test;
 public class TestProgram {
 
 	public static void main(String[] args) {
-		System.out.println("Sample program");
+		System.out.println("Sample program...");
 
 	}
 
